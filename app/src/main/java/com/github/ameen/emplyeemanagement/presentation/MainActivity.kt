1c5b5f9124e3d0aef0160287fe74baf09d636f27
AmeenAhmed1 <ameen.mobiledev@gmail.com>
2022-12-02 13:49:08 +0200

    Add HiltAndroid Application EntryPoint

--- app/src/main/java/com/github/ameen/emplyeemanagement/presentation/MainActivity.kt
+++ app/src/main/java/com/github/ameen/emplyeemanagement/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.github.ameen.emplyeemanagement
+package com.github.ameen.emplyeemanagement.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.ameen.emplyeemanagement.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
