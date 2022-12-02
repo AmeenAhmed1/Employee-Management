6c498ebc0cc75fb2202dfdd00dc84c3f072f2885
AmeenAhmed1 <ameen.mobiledev@gmail.com>
2022-12-02 12:59:32 +0200

    LocalDatabase for Employess-Skills with DAO

---
+++ app/src/main/java/com/github/ameen/emplyeemanagement/data/local/dao/SkillsDao.kt
@@ -0,0 +1,20 @@
+package com.github.ameen.emplyeemanagement.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.github.ameen.emplyeemanagement.data.local.entity.SkillsEntity
+
+@Dao
+interface SkillsDao {
+
+    @Insert
+    suspend fun insertSkills(skillsData: List<SkillsEntity>): List<Long>
+
+    @Query("SELECT * FROM SkillsTable WHERE skillId =:skillId")
+    suspend fun getSpecificSkill(skillId: Int): SkillsEntity
+
+    @Query("SELECT * FROM SkillsTable")
+    suspend fun getAllSkills(): List<SkillsEntity>
+
+}
---
+++ app/src/main/java/com/github/ameen/emplyeemanagement/data/local/entity/EmployeeEntity.kt
@@ -0,0 +1,14 @@
+package com.github.ameen.emplyeemanagement.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "EmployeeTable")
+data class EmployeeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val employeeId: Int? = null,
+    val employeeName: String,
+    val employeeEmail: String? = null,
+    val employeeImage: String? = null,
+    val employeeSkills: List<Int>? = null
+)
